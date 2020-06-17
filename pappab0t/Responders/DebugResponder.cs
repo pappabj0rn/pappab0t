7478e8d103a72c5eb7fd8df5d3e13cace1efd2b9
Greger Olofsson <greger.olofsson@gmail.com>
2020-06-17 08:16:34 +0200

    Simple debug responder

---
+++ pappab0t/Responders/DebugResponder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using MargieBot;
+
+namespace pappab0t.Responders
+{
+    public class DebugResponder : ResponderBase
+    {
+        public override bool CanRespond(ResponseContext context)
+        {
+            Init(context);
+
+            return CommandData.Command == "debug";
+        }
+
+        public override BotMessage GetResponse(ResponseContext context)
+        {
+            Init(context);
+
+            var sb = new StringBuilder();
+
+            if (CommandData.Params.ContainsKey("channels"))
+            {
+                var channelNames = context.Get<Dictionary<string, string>>(Keys.StaticContextKeys.ChannelsNameCache);
+                sb.AppendLine("Channels");
+                foreach (var channelName in channelNames)
+                {
+                    sb.AppendLine($"{channelName.Key}, {channelName.Value}");
+                }
+            }
+
+
+            return new BotMessage
+            {
+                Text = sb.ToString()
+            };
+        }
+    }
+}
